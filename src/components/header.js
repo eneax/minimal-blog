8d0cd3713087b6e2cbf8e6a28c931e695ecc8067
eneax <19818937+eneax@users.noreply.github.com>
2018-07-19 17:44:46 +0200

    Setup Nav

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,14 @@ const Header = ({ siteTitle }) => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: '1.45rem'
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '1.45rem 1.0875rem'
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -20,13 +20,26 @@ const Header = ({ siteTitle }) => (
           to="/"
           style={{
             color: 'white',
-            textDecoration: 'none',
+            textDecoration: 'none'
           }}
         >
           {siteTitle}
         </Link>
       </h1>
     </div>
+
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>
+            Home
+          </Link>
+          <Link to='/about'>
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
   </div>
 )
 
---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render () {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          Plano hell of keytar letterpress, seitan small batch before they
+          sold out authentic tattooed church-key. Poke vape fam 8-bit cloud
+          bread. Everyday carry hammock church-key pickled cold-pressed
+          listicle flannel try-hard edison bulb tofu. Irony etsy YOLO yuccie
+          kinfolk man bun drinking vinegar glossier migas occupy before they
+          sold out vaporware keffiyeh. Bitters cred copper mug cray, hexagon
+          yr ethical meditation meggings seitan wayfarers.
+        </p>
+      </div>
+    )
+  }
+}
