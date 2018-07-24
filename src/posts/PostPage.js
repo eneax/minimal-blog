c5c13f0a2101e2ba3c8c8b0b3a0de5e8304e6b35
eneax <19818937+eneax@users.noreply.github.com>
2018-07-24 16:58:44 +0200

    Style PostPage with styled-components

--- src/posts/PostPage.js
+++ src/posts/PostPage.js
@@ -1,4 +1,53 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+`
+
+const Row = styled.div`
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+`
+
+const Col = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+  @media (min-width: 992px) {
+    -ms-flex: 0 0 66.666667%;
+    flex: 0 0 66.666667%;
+    max-width: 66.666667%;
+  }
+  @media (min-width: 768px) {
+    -ms-flex: 0 0 83.333333%;
+    flex: 0 0 83.333333%;
+    max-width: 83.333333%;
+  }
+  margin-right: auto !important;
+  margin-left: auto !important;
+  h1 {
+    font-size: 30px;
+    margin-bottom: 10px;
+  }
+  @media only screen and (min-width: 768px) {
+    h1 { font-size: 36px; }
+  }
+  p {
+    line-height: 1.5;
+    margin: 30px 0;
+  }
+`
 
 export default class PostPage extends Component {
   render() {
@@ -6,13 +55,17 @@ export default class PostPage extends Component {
     if (!data) return null    
 
     return (
-      <div>
-        <span>{data.markdownRemark.frontmatter.date}</span>
-        <h1>{data.markdownRemark.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{
-          __html: data.markdownRemark.html
-        }} />
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <span>{data.markdownRemark.frontmatter.date}</span>
+            <h1>{data.markdownRemark.frontmatter.title}</h1>
+            <p dangerouslySetInnerHTML={{
+              __html: data.markdownRemark.html
+            }} />
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
@@ -32,3 +85,20 @@ export const query = graphql`
     }
   }
 `
+
+
+
+/*
+
+return (
+      <div>
+        <span>{data.markdownRemark.frontmatter.date}</span>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{
+          __html: data.markdownRemark.html
+        }} />
+      </div>
+    )
+
+
+*/ 
